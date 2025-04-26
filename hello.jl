fe8a72ef073afee0b5cccd1144f9a03158dd33ea
Genki Kajitani <kajikajigenki.1515@gmail.com>
2025-04-26 18:22:51 +0900

    julia追加 formatとエラーハンドリング

---
+++ hello.jl
@@ -0,0 +1,19 @@
+@time println("Hello, World!")
+@time println("This is a Julia script.")
+
+function fibonacci(n)
+  a::BigInt = b::BigInt = 1
+  # a = b = 1
+  for i in 3:n
+    a, b = b, a + b
+  end
+  return b
+end
+
+@time a = fibonacci(1000)
+@time println(a, "\n")
+
+println(pwd())
+parma = () -> ccall((:main, "./libsimple.so"), Cint, ())
+ret = parma()
+println(ret)
