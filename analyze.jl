619b1eec63334e6830d99f43d6e2270633d09d9c
Genki Kajitani <kajikajigenki.1515@gmail.com>
2025-05-08 19:21:33 +0900

    analyze.jl

--- analyze.jl
+++ analyze.jl
@@ -1,39 +1,13 @@
-include("./modules/parma_wrapper.jl")
-using .Parma
+include("./modules/flux_heatmap.jl")
 
-ip = 0            # Particle ID (Particle ID, 0:neutron, 1-28:H-Ni, 29-30:muon+-, 31:e-, 32:e+, 33:photon)
-e = 100.0        # Energy (MeV/n)
-iyear = 2019      # Year
-imonth = 2        # Month
-iday = 1          # Day
-glat = 30.5    # Latitude (deg), -90 =< glat =< 90
-glong = -76.2  # Longitude (deg), -180 =< glong =< 180
-alti = 0.0     # Altitude (km)
-g = 0.15       # Local geometry parameter, 0=< g =< 1: water weight fraction, 10:no-earth, 100:blackhole, -10< g < 0: pilot, g < -10: cabin
-ang = -0.5     # cosine of zenith angle (e.g. ang=1.0 for vertical direction, ang=0.0 for holizontal direction)
+using .FluxPlot
+using CairoMakie
 
-# calculate parameters
-s = getHP(iyear, imonth, iday)  # W-index (solar activity)
-r = getr(glat, glong)           # Vertical cut-off rigidity (GV)
-d = getd(alti, glat)            # Atmospheric depth (g/cm2), set glat = 100 for use US Standard Atmosphere 1976.
+fig = Figure(size=(1500, 800), fontsize=12)
+lat = range(-180, stop=45, length=300)
+lon = range(-120, stop=80, length=300)
+alti = range(0, stop=81, length=9)
 
-Flux, DifFlux = 0.0, 0.0
+title = L"\mathrm{Angular\ integrated\ flux\ (/cm^2/s/(MeV/n))}"
 
-Flux = getSpec(ip, s, r, d, e, g)
-println("Angular Integrated Flux(/cm2/s/(MeV/n))= ", Flux)
-
-glat=-3:0.2:3
-glong=-3:0.2:5
-f(lat, long) = @. getSpec(ip, s, getr(lat, long'), d, e, g)
-z=f(glat, glong)
-
-using Plots
-# gr()
-pyplot()
-p = heatmap(z, 
-    title="Angular Integrated Flux(/cm2/s/(MeV/n))", 
-    xlabel="Longitude (deg)", ylabel="Latitude (deg)", 
-    color=:viridis,  
-    size=(800, 600))
-plot(p, fmt=:png)
-savefig(p, "flux2.png")
+plot_coordinate33!(fig, lat, lon, alti, title=title)
--- modules/flux_heatmap.jl
+++ modules/flux_heatmap.jl
@@ -1,3 +1,6 @@
+module FluxPlot
+export plot_coordinate33!
+
 include("./parma_wrapper.jl")
 using .Parma
 using CairoMakie
@@ -8,49 +11,52 @@ using Printf
 
 ip = 0            # Particle ID (Particle ID, 0:neutron, 1-28:H-Ni, 29-30:muon+-, 31:e-, 32:e+, 33:photon)
 e = 100.0        # Energy (MeV/n)
-iyear = 2019      # Year
+iyear = 2018      # Year
 imonth = 2        # Month
 iday = 1          # Day
 g = 0.15       # Local geometry parameter, 0=< g =< 1: water weight fraction, 10:no-earth, 100:blackhole, -10< g < 0: pilot, g < -10: cabin
 ang = -0.5     # cosine of zenith angle (e.g. ang=1.0 for vertical direction, ang=0.0 for holizontal direction)
 
-lat = range(-90, stop=90, length=300)
-lon = range(-180, stop=180, length=300)
-title = L"\mathrm{Angular\ integrated\ flux\ (/cm^2/s/(MeV/n))}"
-fig = Figure(title=title, size=(1500, 800), fontsize=12)
-hms = Heatmap[]
-flux_maxs = []
 
-for i in 1:3, j in 1:3
-  alti = 9. * ((i - 1) + 3 * (j - 1)) # altitude in km
-  flux = @. getSpec(ip, s, getr(lat', lon), getd(alti, lat'), e, g)
-  push!(flux_maxs, maximum(flux))
-  
-  ax = Axis(fig[i, j], title="altitude = $(alti) km")
-  hm = heatmap!(ax, lon, lat, flux; colormap=:inferno)
-  colsize!(fig.layout, j, Aspect(1, 1.6))
-  hidedecorations!(ax)
-  if i != 3
-    ax.xticksvisible = false
-    ax.xticklabelsvisible = false
+function plot_coordinate33!(fig, latitude, longitude, altitude; title="")
+  s = getHP(iyear, imonth, iday) # W-index (solar activity)
+
+  hms = Heatmap[]
+  flux_maxs = []
+
+  for i in 1:3, j in 1:3
+    alti = altitude[(j - 1) * 3 + i]
+    flux = @. getSpec(ip, s, getr(latitude', longitude), getd(alti, latitude'), e, g)
+    push!(flux_maxs, maximum(flux))
+
+    ax = Axis(fig[i, j], title="altitude = $(alti) km")
+    hm = heatmap!(ax, longitude, latitude, flux; colormap=:inferno)
+    colsize!(fig.layout, j, Aspect(1, 1.6))
+    if i != 3
+      ax.xticksvisible = false
+      ax.xticklabelsvisible = false
+    end
+    if j != 1
+      ax.yticksvisible = false
+      ax.yticklabelsvisible = false
+    end
+    ax.xticks = -180:60:180
+    ax.yticks = -90:30:90
+    push!(hms, hm)
   end
-  if j != 1
-    ax.yticksvisible = false
-    ax.yticklabelsvisible = false
+  for i in 1:9
+    flux_max = maximum(flux_maxs)
+    hms[i].colorrange = (0, flux_max)
   end
-  ax.xticks = -180:60:180
-  ax.yticks = -90:30:90
-  push!(hms, hm)
+
+  rowgap!(fig.layout, 10)
+  colgap!(fig.layout, 10)
+
+  Colorbar(fig[:, end+1], hms[1], label="Flux", labelrotation=π / 2)
+  Label(fig[0, :], title, fontsize=22)
+  resize_to_layout!(fig)
+  fig
 end
-for i in 1:9
-  flux_max = maximum(flux_maxs)
-  hms[i].colorrange = (0, flux_max)
 end
 
-rowgap!(fig.layout, 10)
-colgap!(fig.layout, 10)
 
-Colorbar(fig[:, end+1], hms[1], label="Flux", labelrotation=π / 2)
-Label(fig[0, :], title, fontsize=22)
-resize_to_layout!(fig)
-fig
